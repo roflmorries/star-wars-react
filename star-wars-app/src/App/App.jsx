bb2a2755744b7d2fcf42c48aca0045330d1d9c6f
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-03-23 19:19:40 +0200

    organized structure

---
+++ star-wars-app/src/App/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div id='stars'></div>
+    <div id='stars2'></div>
+    <div id='stars3'></div>
+      
+    </>
+  )
+}
+
+export default App
